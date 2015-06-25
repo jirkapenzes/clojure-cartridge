f3d1f58ebfb817213d2c0499b3df792704b3dbcb
jirkapenzes <jirkapenzes@gmail.com>
2015-06-25 08:45:35 +0200

    Updated template

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,6 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
-    :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
+    :plugins [[lein-ring "0.9.4"]]
+    :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                   [aleph "0.4.0"]]
+                   
     :main webapp)
--- template/src/webapp.clj
+++ template/src/webapp.clj
@@ -1,12 +1,13 @@
 (ns webapp
-  (:use org.httpkit.server))
+  (:require [aleph.http :as http]))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+   :body "Hello world!"})
 
 (defn -main [& args]
   (let [port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))
-        ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")]
-    (run-server app {:ip ip :port port})))
+        ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")
+        socket-address (java.net.InetSocketAddress. (java.net.InetAddress/getByName ip) port) ]
+    (http/start-server app {:socket-address socket-address})))
